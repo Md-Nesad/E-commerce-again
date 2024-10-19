d55e57b03b4a71354be566bb72267b408b3ea1f1
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2024-10-19 20:23:35 +0600

    31

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import MyState from "./context/myState";
 import { Toaster } from "react-hot-toast";
 import { ProtectedRouteForUser } from "./protectedRoute/ProtectedRouteForUser";
 import { ProtectedRouteForAdmin } from "./protectedRoute/ProtectedRouteForAdmin";
+import CategoryPage from "./pages/category/CategoryPage";
 
 const App = () => {
   return (
@@ -24,11 +25,16 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
-          <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/allproduct" element={<AllProduct />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/category/:categoryname"
+            element={<CategoryPage />}
+          />{" "}
+          {/* category Page route  */}
           <Route
             path="/user-dashboard"
             element={
@@ -54,7 +60,7 @@ const App = () => {
             }
           />
           <Route
-            path="/updateproduct"
+            path="/updateproduct/:id"
             element={
               <ProtectedRouteForAdmin>
                 <UpdateProductPage />
