24804cf2d7d0eb7b3e86ea68ab7124297f9d6346
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2024-10-21 00:23:29 +0600

    firebase problem

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,4 +5,8 @@ export const store = configureStore({
   reducer: {
     cart: cartSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
