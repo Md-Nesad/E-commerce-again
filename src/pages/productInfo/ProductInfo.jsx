045d836eecce9f973fd1108faa2fd197b8d6076b
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2024-11-14 12:40:52 +0600

    buy now

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -5,6 +5,9 @@ import { useParams } from "react-router";
 import { fireDB } from "../../firebase/FirebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 import Loader from "../../components/loader/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, deleteFromCart } from "../../redux/cartSlice";
+import toast from "react-hot-toast";
 
 const ProductInfo = () => {
   const context = useContext(myContext);
@@ -13,21 +16,39 @@ const ProductInfo = () => {
   const [product, setProduct] = useState("");
 
   const { id } = useParams();
-
-  // console.log(product)
-
   // getProductData
   const getProductData = async () => {
     setLoading(true);
     try {
       const productTemp = await getDoc(doc(fireDB, "products", id));
-      setProduct(productTemp.data());
+      // console.log({...productTemp.data(), id : productTemp.id})
+      setProduct({ ...productTemp.data(), id: productTemp.id });
       setLoading(false);
     } catch (error) {
       console.log(error);
       setLoading(false);
     }
   };
+
+  const cartItems = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const addCart = (item) => {
+    // console.log(item)
+    dispatch(addToCart(item));
+    toast.success("Add to cart");
+  };
+
+  const deleteCart = (item) => {
+    dispatch(deleteFromCart(item));
+    toast.success("Delete cart");
+  };
+
+  // console.log(cartItems)
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   useEffect(() => {
     getProductData();
@@ -135,8 +156,25 @@ const ProductInfo = () => {
 
                     <div className="mb-6 " />
                     <div className="flex flex-wrap items-center mb-6">
-                      <button className="w-full px-4 py-3 text-center text-pink-600 bg-pink-100 border border-pink-600  hover:bg-pink-600 hover:text-gray-100  rounded-xl">
-                        Add to cart
+                      {cartItems.some((p) => p.id === product.id) ? (
+                        <button
+                          onClick={() => deleteCart(product)}
+                          className="w-full px-4 py-3 text-center text-white bg-red-500 border border--600  hover:bg-red-600 hover:text-gray-100  rounded-xl"
+                        >
+                          Delete to cart
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => addCart(product)}
+                          className="w-full px-4 py-3 text-center text-pink-600 bg-pink-100 border border-pink-600  hover:bg-pink-600 hover:text-gray-100  rounded-xl"
+                        >
+                          Add to cart
+                        </button>
+                      )}
+                    </div>
+                    <div className="flex gap-4 mb-6">
+                      <button className="w-full px-4 py-3 text-center text-gray-100 bg-pink-600 border border-transparent dark:border-gray-700 hover:border-pink-500 hover:text-pink-700 hover:bg-pink-100 rounded-xl">
+                        Buy now
                       </button>
                     </div>
                   </div>
